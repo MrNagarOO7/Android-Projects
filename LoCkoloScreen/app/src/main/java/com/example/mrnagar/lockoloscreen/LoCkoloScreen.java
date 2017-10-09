c1ff50aad47b82c97db30753abac41a9262fed5d
MrNagarOO7 <nagaryash61@gmail.com>
2017-10-09 21:01:18 +0530

    LoCkoloScreen - Custom Lockscreen.

---
+++ LoCkoloScreen/app/src/main/java/com/example/mrnagar/lockoloscreen/LoCkoloScreen.java
@@ -0,0 +1,11 @@
+package com.example.mrnagar.lockoloscreen;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoCkoloScreen extends AppCompatActivity {
+
+    
+}
+
